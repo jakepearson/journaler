665ab4bf8b9e246c24aa68e9f7e76b37242a0c0d
Jake Pearson <jpearson@rallydev.com>
2014-01-06 18:23:18 -0700

    Set minimum lein version

--- project.clj
+++ project.clj
@@ -3,11 +3,10 @@
   :url "https://github.com/RallySoftware/journaler"
   :license {:name "Eclipse 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [slingshot "0.10.3"]
                  [cheshire "5.0.1"]
                  [compojure "1.1.5"]
                  [http-kit "2.1.16"]]
-
+  :min-lein-version "2.0.0"
   :main journaler.handler)
